4d681908d2e54e50e4e5f40ebdf75977024dcce9
Pranay Shah <pranayshah011@gmail.com>
2021-03-07 16:38:25 +0530

    update 5

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ File to store updates made during the development of this project.
 update 2: added guild name and printing of guild members code. enabled members intent.  
 update 3: happy birthday feature and on_message error handling added.  
 update 4: converted to a bot event handler and added slap feature.  
+update 5: made an anime search command which connects to anilist and prints out details of search parameter.  
--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 import os
-from discord import Member
+from discord import Member,Embed
 from typing import Optional
 import random
+import requests
+
 from discord.ext import commands
 from discord.ext.commands import BadArgument
+from bs4 import BeautifulSoup
+from markdown import markdown
 
 #intents = discord.Intents.default()
 #intents.members = True
 from dotenv import load_dotenv
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -16,25 +19,60 @@ GUILD = os.getenv('DISCORD_GUILD')
 #client = discord.Client(intents=intents)
 bot= commands.Bot(command_prefix="!")
 
-@bot.command(name='99',help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        )
-    ]
+@bot.command(name='anidex')
+async def anime_title(ctx, *,name):
+    query='''
+    query($aniname: String){
+        Media (search: $aniname, type: ANIME){
+            id
+            title {
+                romaji
+                english
+            }
+            status
+            description
+            episodes
+            meanScore
+            tags{
+                name
+                isMediaSpoiler
+            }
+        }
+    }
+    '''
+    variables = {
+        'aniname': name
+    }
+    url = 'https://graphql.anilist.co'
+    response = requests.post(url, json={'query': query, 'variables': variables})
+    data = response.json()
+    if("errors" in data):
+        for item in data["errors"]:
+            await ctx.send(item["message"])
+        return
+    if(data["data"]["Media"]["title"]["english"] is not None): 
+        tit=data["data"]["Media"]["title"]["english"]+"\n"+data["data"]["Media"]["title"]["romaji"]
+    else:
+        tit=data["data"]["Media"]["title"]["romaji"]
+    embedVar=Embed(title=tit)
+    if(type(data["data"]["Media"]["description"] is not None)):
+        html=markdown(data["data"]["Media"]["description"])
+        soup = BeautifulSoup(html, "html.parser")
+        text=soup.get_text()
+        embedVar.add_field(name="Synopsis:", value=text, inline=False)
+    if(data["data"]["Media"]["status"] is not None):
+        embedVar.add_field(name="Staus:", value=data["data"]["Media"]["status"].lower(), inline=True)
+    if(data["data"]["Media"]["episodes"] is not None):
+        embedVar.add_field(name="Episode Count:", value=data["data"]["Media"]["episodes"], inline=True)
+    if(data["data"]["Media"]["meanScore"] is not None):
+        embedVar.add_field(name="Mean Score:", value=data["data"]["Media"]["meanScore"], inline=True)
+    taglst=[]
+    for item in data["data"]["Media"]["tags"]:
+        if(not item["isMediaSpoiler"]):
+            taglst.append(item["name"])
+    if(not len(taglst)==0):
+        embedVar.add_field(name="Tags:", value=", ".join(taglst), inline=True)
+    await ctx.send(embed=embedVar)
 
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-@bot.command(name='slap', aliases=['hit'],help='slaps person tagged for added reason(optional)')
-async def slap_member(ctx, member: Member, *, reason: Optional[str]="for no reason"):
-    await ctx.send(f"{ctx.author.display_name} slapped {member.mention} {reason}!")
-@slap_member.error
-async def slap_member_error(ctx,exc):
-    if isinstance(exc, BadArgument):
-        await ctx.send("I can't find that member.")
 
 bot.run(TOKEN)
